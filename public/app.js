826b9dee3cedc959bae879be93177f5cbc4abf68
subspacesignal <signalsubspace@gmail.com>
2017-02-02 18:36:29 +0000

    Showing thumbnails on index page

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ var app = angular.module('app', []);
 
 app.controller('mainController', ['$scope', '$http', function ($scope, $http) {
   $http.get('/api').then(function successCallback(response) {
-    $scope.hrefs = response.data;
+    $scope.hrefs = response.data.hrefs;
   }, function errorCallback(response) {
     console.log(response);
   });
